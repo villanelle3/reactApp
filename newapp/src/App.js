2da453cee3743d32ed0802c84617d60083d6c1de
Laura Augusta <laurah.algusta@gmail.com>
2023-03-03 15:50:31 -0300

    imitial commit

---
+++ newapp/src/App.js
@@ -0,0 +1,24 @@
+import "./styles/index.scss"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+          <h1>teste</h1>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
